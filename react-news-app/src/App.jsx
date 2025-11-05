5ed6ca2e0b4e353de4e611f4d85dedcf02a5b9c6
Viji Ramu <vijiramu@gmail.com>
2025-11-05 12:17:59 -0600

    Featue started

---
+++ react-news-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import News from './components/News'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className='container'>
+        <div className="news-app">
+        
+         <News />   
+           
+        </div>
+  </div>
+  )
+}
+
+export default App
